131960dc67b7331d9b83394746b24f2cd6fb7504
Christopher Townson <iteratoruk@gmail.com>
2018-10-29 15:24:03 +0000

    CDSID-296 implement deserialization model for declaration status API response message

---
+++ src/main/scala/uk/gov/hmrc/wco/dec/status/StatusRequest.scala
@@ -0,0 +1,111 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.wco.dec.status
+
+import com.fasterxml.jackson.core.JsonParser
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer
+import com.fasterxml.jackson.databind.node.{ObjectNode, TextNode}
+import com.fasterxml.jackson.databind.{DeserializationContext, JsonNode}
+import com.fasterxml.jackson.dataformat.xml.annotation.{JacksonXmlProperty, JacksonXmlRootElement, JacksonXmlText}
+import uk.gov.hmrc.wco.dec.{JacksonMapper, StdAttributeAndTextDeserializer}
+
+@JacksonXmlRootElement(namespace = "", localName = "declarationManagementInformationResponse")
+case class DeclarationManagementInformationResponse(declaration: Declaration = Declaration())
+
+object DeclarationManagementInformationResponse extends JacksonMapper {
+
+  def fromXml(xml: String): DeclarationManagementInformationResponse = _xml.readValue(xml, classOf[DeclarationManagementInformationResponse])
+
+}
+
+case class Declaration(versionNumber: Option[Int] = None,
+                       creationDate: Option[DateTime] = None,
+                       goodsItemCount: Option[Quantity] = None,
+                       tradeMovementType: Option[CodeListElementField] = None,
+                      `type`: Option[CodeListElementField] = None,
+                       packageCount: Option[Quantity] = None,
+                       acceptanceDate: Option[DateTime] = None,
+                       parties: Seq[Parties] = Seq.empty)
+
+// TODO implement function in DateTime to retrieve value as actual Date when we understand "formatCode" better
+@JsonDeserialize(using = classOf[DateTimeDeserializer])
+case class DateTime(@JacksonXmlProperty(isAttribute = true)
+                    formatCode: Option[String] = None,
+
+                    @JacksonXmlText
+                    value: Option[String] = None)
+
+class DateTimeDeserializer extends StdAttributeAndTextDeserializer[DateTime]("formatCode", classOf[DateTime]) {
+
+  override def newInstanceFromTuple(values: (Option[String], Option[String])): DateTime = DateTime(values._1, values._2)
+
+}
+
+@JsonDeserialize(using = classOf[QuantityDeserializer])
+case class Quantity(@JacksonXmlProperty(isAttribute = true)
+                    unitType: Option[String] = None,
+
+                    @JacksonXmlProperty(isAttribute = true)
+                    qualifier: Option[String] = None,
+
+                    @JacksonXmlText
+                    value: Option[String] = None)
+
+class QuantityDeserializer extends ExtendedAttributeAndTextDeserializer[Quantity]("unitType", "qualifier", classOf[Quantity]) {
+
+  override def newInstanceFromTuple(values: (Option[String], Option[String], Option[String])): Quantity = Quantity(values._1, values._2, values._3)
+
+}
+
+@JsonDeserialize(using = classOf[CodeListElementFieldDeserializer])
+case class CodeListElementField(@JacksonXmlProperty(isAttribute = true)
+                                `type`: Option[String] = None,
+
+                                 @JacksonXmlProperty(isAttribute = true)
+                                 responsibleAgent: Option[String] = None,
+
+                                 @JacksonXmlText
+                                 value: Option[String] = None)
+
+class CodeListElementFieldDeserializer extends ExtendedAttributeAndTextDeserializer[CodeListElementField]("type", "responsibleAgent", classOf[CodeListElementField]) {
+
+  override def newInstanceFromTuple(values: (Option[String], Option[String], Option[String])): CodeListElementField = CodeListElementField(values._1, values._2, values._3)
+
+}
+
+case class Parties(partyIdentification: Option[PartyIdentification] = None)
+
+case class PartyIdentification(number: Option[String] = None)
+
+// yes, I know this is crude and clumsy duplication but it will have to suffice for now
+// TODO create a more elegant solution which refactors StdAttributeAndTextDeserializer to handle more than 1 attribute
+abstract class ExtendedAttributeAndTextDeserializer[T](attributeOne: String, attributeTwo: String, t: Class[T]) extends StdDeserializer[T](t) {
+
+  def newInstanceFromTuple(values: (Option[String], Option[String], Option[String])): T
+
+  override final def deserialize(p: JsonParser, ctx: DeserializationContext): T = {
+    val n: JsonNode = p.getCodec.readTree(p)
+    n match {
+      case o: ObjectNode => newInstanceFromTuple((nonEmptyOrNone(o.get(attributeOne)), nonEmptyOrNone(o.get(attributeTwo)), nonEmptyOrNone(o.get(""))))
+      case t: TextNode => newInstanceFromTuple((None, None, nonEmptyOrNone(t)))
+    }
+  }
+
+  private def nonEmptyOrNone(n: JsonNode): Option[String] = if (n == null || n.asText() == null || n.asText().trim.isEmpty) None else Some(n.asText())
+
+}
---
+++ src/test/scala/uk/gov/hmrc/wco/dec/status/StatusRequestSpec.scala
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2018 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.wco.dec.status
+import uk.gov.hmrc.wco.dec.WcoSpec
+
+import scala.xml.XML
+
+class StatusRequestSpec extends WcoSpec {
+
+  "from XML" should {
+
+    val xml = XML.load(getClass.getResourceAsStream("/StatusQueryResponseExample.xml")).mkString
+    val resp = DeclarationManagementInformationResponse.fromXml(xml)
+
+    "read declaration version number" in {
+      resp.declaration.versionNumber.get must be("100")
+    }
+
+    "read creation date format code" in {
+      resp.declaration.creationDate.get.formatCode.get must be("string")
+    }
+
+    "read creation date value" in {
+      resp.declaration.creationDate.get.value.get must be("2008-09-29T02:49:45")
+    }
+
+    "read goods item count unit type" in {
+      resp.declaration.goodsItemCount.get.unitType.get must be("string")
+    }
+
+    "read goods item count qualifier" in {
+      resp.declaration.goodsItemCount.get.qualifier.get must be("string")
+    }
+
+    "read goods item count value" in {
+      resp.declaration.goodsItemCount.get.value.get must be("1000.00")
+    }
+
+    "read trade movement type type" in {
+      resp.declaration.tradeMovementType.get.`type`.get must be("token")
+    }
+
+    "read trade movement type responsible agent" in {
+      resp.declaration.tradeMovementType.get.responsibleAgent.get must be("token")
+    }
+
+    "read trade movement type value" in {
+      resp.declaration.tradeMovementType.get.value.get must be("token")
+    }
+
+    "read type type" in {
+      resp.declaration.`type`.get.`type`.get must be("token")
+    }
+
+    "read type responsible agent" in {
+      resp.declaration.`type`.get.responsibleAgent.get must be("token")
+    }
+
+    "read type value" in {
+      resp.declaration.`type`.get.value.get must be("token")
+    }
+
+    "read package count unit type" in {
+      resp.declaration.packageCount.get.unitType.get must be("string")
+    }
+
+    "read package count qualifier" in {
+      resp.declaration.packageCount.get.qualifier.get must be("string")
+    }
+
+    "read package count value" in {
+      resp.declaration.packageCount.get.value.get must be("1000.00")
+    }
+
+    "read acceptance date format code" in {
+      resp.declaration.acceptanceDate.get.formatCode.get must be("string")
+    }
+
+    "read acceptance date value" in {
+      resp.declaration.acceptanceDate.get.value.get must be("2014-09-19T00:18:33")
+    }
+
+    "read parties party identification number" in {
+      resp.declaration.parties.head.partyIdentification.get.number.get must be("string")
+    }
+
+  }
+
+}
