7173180ebf8afc124093e3ed214a6b980d6bd6e7
matt.clark <36630558+mattclark-zerogravit@users.noreply.github.com>
2019-04-15 15:12:27 +0100

    CEDS-1102: old wco-dec version

--- updateSchema/update.sh
+++ updateSchema/update.sh
@@ -1,13 +1,26 @@
-#!/usr/bin/bash
+#!/bin/bash
 
 
 # Delete previous schemas
-rm -f *.xsd *.xsd.*
+rm -rf customs-declarations/
 
 # Download the latest Shcema definitions
-wget https://github.com/hmrc/customs-declarations/blob/master/public/api/conf/1.0/schemas/wco/declaration/DocumentMetaData_2_DMS.xsd
-wget https://github.com/hmrc/customs-declarations/blob/master/public/api/conf/1.0/schemas/wco/declaration/WCO_DEC_2_DMS.xsd
-
+git clone git@github.com:hmrc/customs-declarations.git
+cd customs-declarations
+git checkout v0.245.0
+cd ..
 # Generate the WCO-DEC model
 
+mkdir wco
 
+
+xjc -d wco ./customs-declarations/public/api/conf/1.0/schemas/wco/declaration/DocumentMetaData_2_DMS.xsd
+xjc -d wco ./customs-declarations/public/api/conf/1.0/schemas/wco/declaration/WCO_DEC_2_DMS.xsd
+
+# move generated file to source folder
+
+mv  wco/* ../src/main/java/
+
+
+rm -rf customs-declarations/
+rm -rf wco
